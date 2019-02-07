637a666b180333c30183cce654c4e56cc064d575
pirupius <piruville@gmail.com>
2019-02-07 19:38:21 +0300

    passing data in routes

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
 app.config['TESTING'] = True
 app.config['FLASK_DEBUG'] = True
-app.config['FLASK_ENV'] = development
+app.config['FLASK_ENV'] = 'development'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+Articles = Articles()
 
 @app.route('/')
 def index():
@@ -12,7 +15,11 @@ def index():
 
 @app.route('/articles')
 def articles():
-    return render_template('articles.html')
+    return render_template('articles.html', articles = Articles)
+
+@app.route('/article/<string:id>')
+def article(id):
+    return render_template('article.html', id = id)
 
 if __name__ == '__main__':
     app.run()
--- data.py
+++ data.py
@@ -3,16 +3,23 @@ def Articles():
         {
             'id': 1,
             'title': 'First Article',
-            'body': 'Commodo cupidatat et cillum excepteur qui Lorem Lorem non ex exercitation proident sit sint. Occaecat non quis ea aute ut adipisicing elit sint. Culpa cillum consequat qui officia minim eiusmod. Commodo cillum est pariatur culpa sint tempor cupidatat tempor deserunt ipsum incididunt sunt enim. Sunt consectetur amet cillum occaecat in reprehenderit proident consequat occaecat. Est magna irure veniam ex consequat non cupidatat officia dolor.'
+            'body': 'Commodo cupidatat et cillum excepteur qui Lorem Lorem non ex exercitation proident sit sint. Occaecat non quis ea aute ut adipisicing elit sint. Culpa cillum consequat qui officia minim eiusmod. Commodo cillum est pariatur culpa sint tempor cupidatat tempor deserunt ipsum incididunt sunt enim. Sunt consectetur amet cillum occaecat in reprehenderit proident consequat occaecat. Est magna irure veniam ex consequat non cupidatat officia dolor.',
             'author': 'Bruce Wayne',
             'created_at': '01-02-2019'
         },
         {
-            'id': 1,
-            'title': 'First Article',
-            'body': ''
-            'author': 'Bruce Wayne',
-            'created_at': '01-02-2019'
+            'id': 2,
+            'title': 'Am the coolest',
+            'body': 'Deserunt proident laborum non nostrud laborum sint do. Incididunt laborum exercitation ea esse pariatur ex cillum veniam sit irure eiusmod deserunt et sint. Adipisicing dolore dolore nostrud reprehenderit voluptate irure Lorem nulla enim. Nisi in Lorem veniam culpa ipsum.',
+            'author': 'Aquaman',
+            'created_at': '21-01-2019'
         },
-        
+        {
+            'id': 3,
+            'title': 'I should be an Avenger',
+            'body': 'Consequat anim proident consectetur tempor id dolore. Laboris cupidatat voluptate do labore sit minim. Enim ipsum et dolore cupidatat. Reprehenderit velit sint laborum deserunt qui. Incididunt sint laboris ea laborum ullamco consequat aliqua reprehenderit id velit.',
+            'author': 'Wonder Woman',
+            'created_at': '6-02-2019'
+        }
     ]
+    return articles
