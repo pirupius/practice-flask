51caa23908c6d4d975d76bad6cf1c562262071d6
pirupius <piruville@gmail.com>
2019-02-09 11:03:02 +0300

    wip forms

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request  
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 from data import Articles
+from forms import RegisterForm
+
+import pymysql
 
 app = Flask(__name__)
 app.config['TESTING'] = True
@@ -21,5 +26,16 @@ def articles():
 def article(id):
     return render_template('article.html', id = id)
 
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return render_template('register.html')
+    return render_template('register.html', form = form)
+
+
 if __name__ == '__main__':
     app.run()
+
+
+# fuser 5000/tcp -k
